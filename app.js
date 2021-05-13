f4caa34145a4444af93eb23bdd5e88890a4a0c6e
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-05-13 17:36:45 +0300

    feat: add loggers

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const cors = require('cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger'); 
 const nodemailer = require('nodemailer');
 const { body, validationResult } = require('express-validator');
+const cors = require('cors');
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+
 const app = express();
-const port = 3000;
 
 const transporter = nodemailer.createTransport({
   port: 465,
@@ -17,6 +20,8 @@ const transporter = nodemailer.createTransport({
 
 app.use(cors({ origin: ["https://scnonstop.ru", "https://www.scnonstop.ru"] }));
 app.use(express.json());
+
+app.use(requestLogger);
 
 app.post('/api/mail',
   body('appType').isLength({ max: 25 }).not().isEmpty().trim().escape(),
@@ -69,7 +74,8 @@ app.use(function (req, res, next) {
   res.status(404).send('Sorry cant find that!');
 });
 
+app.use(errorLogger);
 
-app.listen(port, () => {
-  console.log(`App is listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`App is listening at http://localhost:${PORT}`)
 })
