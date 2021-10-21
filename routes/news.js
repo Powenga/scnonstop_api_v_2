0d55b3f56a4fad11e3212afaf247369418873c53
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-10-21 20:29:36 +0300

    feat: protect news routes

--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+const auth = require('../middlewares/auth');
 const {
   createNews, updateNewsImage, updateNews, deleteNews, getAllNews,
 } = require('../controllers/news');
 const { validateNews, validateID } = require('../middlewares/validator');
 
 router.get('/', getAllNews);
+
+router.use(auth);
 router.post('/', validateNews, createNews);
 router.patch('/:id/image', validateID, updateNewsImage);
 router.patch('/:id', validateID, validateNews, updateNews);
